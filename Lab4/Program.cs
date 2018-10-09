903c8a54faf4e655824564add3ca6d766ff01d7e
Jordan Owiesny <jordanowiesny@quickenloans.com>
2018-10-09 15:06:05 -0400

    Add project files.

---
+++ Lab4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool s = true;
+            double num = 0.0;
+            while (s)
+            {
+                try
+                {
+                    Console.Write("Gim'me a num: ");
+                    num = double.Parse(Console.ReadLine());
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("SHIT code: " + ex.Message);
+                    continue;
+                }
+                Console.WriteLine("Numah".PadRight(10) + "Square".PadRight(10) + "Cubed".PadRight(10));
+                Console.WriteLine("#####".PadRight(10) + "######".PadRight(10) + "#####".PadRight(10));
+                for (int i = 0; i < num; i++)
+                {
+                    Console.WriteLine( 
+                                     (i + 1).ToString().PadRight(10) +
+                                     (Math.Pow(i + 1, 2)).ToString().PadRight(10) +
+                                     (Math.Pow(i + 1, 3)).ToString().PadRight(10)
+                                     );
+                }
+                Console.Write("Continue (y/n)?: ");
+                s = (Console.ReadLine() == "y")? true: false;
+            }
+        }
+    }
+}
